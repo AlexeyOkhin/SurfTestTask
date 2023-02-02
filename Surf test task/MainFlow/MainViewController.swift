70af8f4eeae2bd1d4deaf67d72f297abf6e345d6
AlexeyOkhin <k-i-ll-e-r@list.ru>
2023-02-02 22:39:39 +0500

    Настроил проект для старта

---
+++ Surf test task/MainFlow/MainViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Surf test task
+//
+//  Created by Djinsolobzik on 02.02.2023.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    //MARK: - Properties
+
+    //MARK: - Private Properties
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .tintColor
+    }
+    //MARK: - Methods
+
+    //MARK: - Privaye Methods
+}
+
+//MARK: - Private Extensions
+
