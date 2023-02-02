c4a6fdc8213294ddf1c4427a475b76e03c53017f
AlexeyOkhin <k-i-ll-e-r@list.ru>
2023-02-02 22:09:48 +0500

    Initial Commit

---
+++ Surf test task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Surf test task
+//
+//  Created by Djinsolobzik on 02.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
