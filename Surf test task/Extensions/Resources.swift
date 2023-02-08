a5b7fe11b3fdc206e7c8b2fb02eb1ca2c6ac1c2a
AlexeyOkhin <k-i-ll-e-r@list.ru>
2023-02-09 00:15:25 +0500

    add upLineCell

--- Surf test task/Extensions/Resources.swift
+++ Surf test task/Extensions/Resources.swift
@@ -12,6 +12,9 @@ enum Resources {
         static var titleText = UIColor(hexString: "#313131")
         static var descriptionText = UIColor(hexString: "#96959B")
         static let buttonSend = UIColor(hexString: "#313131")
+        static let cellDisableColor = UIColor(hexString: "#F3F3F5")
+        static let cellEnableColor = UIColor(hexString: "#313131")
+
     }
 
     enum Font {
